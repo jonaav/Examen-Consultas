182d051f74e873f1bff2aea6bddfbd075f3e4903
jonaav <jonajaav@gmail.com>
2021-06-25 23:20:14 -0500

    cm

--- src/main/java/com/example/examen/repository/IConsultaRepository.java
+++ src/main/java/com/example/examen/repository/IConsultaRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.examen.entity.Consulta;
 
@@ -12,7 +13,7 @@ public interface IConsultaRepository extends JpaRepository<Consulta, Long>{
 
 	@Query(value = "SELECT * FROM consultas c INNER JOIN medicos m WHERE c.id_medico = :idMedico", 
 			nativeQuery = true)
-	public List<Consulta> findByIdMedico(Long idMedico);
+	public List<Consulta> findByIdMedico(@Param("idMedico")Long idMedico);
 	
 	public List<Consulta> findByFechaAndIdPaciente(Date fecha, Long idPaciente);
 }
